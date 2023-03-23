b333a1cc6df98db3dedf629200e928244427182f
Gianx64 <GianPol1337@gmail.com>
2023-03-23 15:03:38 -0700

    Translation fix

--- resources/views/errors.blade.php
+++ resources/views/errors.blade.php
@@ -1,6 +1,6 @@
 @if (count($errors) > 0)
 	<div class="alert alert-danger mt-4">
-		<p>Corrige los siguientes errores:</p>
+		<p>Correct these errors:</p>
 		<ul>
 			@foreach ($errors->all() as $error)
 				<strong><li>{{$error}}</li></strong>
