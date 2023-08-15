d9957bc994765043761922369d92515425fa54a1
Gianx64 <GianPol1337@gmail.com>
2023-08-14 20:24:11 -0400

    Jobs & ApplicationClean

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,9 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            \App\Jobs\ApplicationsClean::dispatch();
+        })->dailyAt('00:00');
     }
 
     /**
