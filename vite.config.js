4880019d61bd6adb1054ffc40958707c124ff783
Gianx64 <gianpol1337@gmail.com>
2023-08-23 19:23:50 -0400

    Livewire + Vite fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import livewire from '@defstudio/vite-livewire-plugin';
 import path from 'path'
 
 export default defineConfig({
+    server: {
+        host: '10.2.139.30'
+    },
     plugins: [
         laravel([
             'resources/js/app.js',
+            {refresh: true,}
         ]),
+        livewire({
+            refresh: ['resources/css/app.css'],
+        }),
     ],
     resolve: {
         alias: {
